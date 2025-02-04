906feb6ca02afee0749586f49f610cb43521501d
Lavanya Palanikumar <lavanyasaii93@gmail.com>
2025-02-03 23:00:41 -0500

    Used set and learned about .join

--- App.py
+++ App.py
@@ -1,21 +1,20 @@
 import random
-from operator import truediv, index
 
 My_List=["Dog","Cat","Mouse"]
 Random_element=random.choice(My_List)
 print(Random_element)
-Guess=0
 
-
-print("_"*len(Random_element))
-value=0
-while Guess<=len(Random_element):
+while len(Random_element)>0:
  user_input = input("Enter a letter: ")
- if Random_element[value]==user_input:
-    print(user_input)
-    value=+ 1
+ used_letters=set(user_input)
+ print("You used this letters: "," ".join(used_letters))
+
+ word_list=[]
+ for letter in Random_element:
+     if letter in used_letters:
+         word_list.append(letter)
+     else:
+         word_list.append("-")
 
 
- else:
-    print("Wrong letter")
-Guess=+1
+
